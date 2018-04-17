34d89e84a628d6db81ee26101f81a1a276a6ff08
Jeffery Wilson <jeff@jeffalwilson.com>
2018-04-17 09:16:10 -0400

    Updates

---
+++ deluge/trigger-decompress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+
+/usr/bin/curl -X POST http://flexget:5050/api/tasks/execute/ -H 'Authorization: Token aeba2402c3894f8ecfcde9b239ab381d234798133717150892cfbaa8' -H 'Content-Type: application/json' -d '{"tasks":["Series-Decompress", "Movies-Decompress"]}'
