366ae74d76d62402e79102a165fd2553797fa869
Jeffery Wilson <jeff@jeffalwilson.com>
2019-11-27 18:53:07 -0500

    Fix decompress script

--- deluge/trigger-decompress.sh
+++ deluge/trigger-decompress.sh
@@ -3,4 +3,4 @@ torrentid=$1
 torrentname=$2
 torrentpath=$3
 
-/usr/bin/curl -X POST http://flexget:5050/api/tasks/execute/ -H 'Authorization: Token aeba2402c3894f8ecfcde9b239ab381d234798133717150892cfbaa8' -H 'Content-Type: application/json' -d '{"tasks":["Series-Decompress", "Movies-Decompress"]}'
+/usr/bin/curl -X POST http://localhost:5050/api/tasks/execute/ -H 'Authorization: Token aeba2402c3894f8ecfcde9b239ab381d234798133717150892cfbaa8' -H 'Content-Type: application/json' -d '{"tasks":["Series-Decompress", "Movies-Decompress"]}'
